35951c6de5a60634f9d2ef28c62c792b54b9aa4e
kazuhira-r <kazuhira.m@gmail.com>
2019-04-21 01:15:56 +0900

    add, melpa-stable

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -19,6 +19,8 @@
 (require 'package)
 (add-to-list 'package-archives '("melpa" .
                                  "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("melpa-stable" .
+                                 "https://stable.melpa.org/packages/"))
 (add-to-list 'package-archives '("marmalade" .
                                  "https://marmalade-repo.org/packages/"))
 (package-initialize)
