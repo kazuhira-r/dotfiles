ed731e636b1ace5f13432a9cfdb29f9bbcc71254
kazuhira-r <kazuhira.m@gmail.com>
2022-12-19 23:55:56 +0900

    remove, marmalade-repo

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -23,8 +23,8 @@
                                  "https://melpa.org/packages/") t)
 (add-to-list 'package-archives '("melpa-stable" .
                                  "https://stable.melpa.org/packages/"))
-(add-to-list 'package-archives '("marmalade" .
-                                 "https://marmalade-repo.org/packages/"))
+;;(add-to-list 'package-archives '("marmalade" .
+;;                                 "https://marmalade-repo.org/packages/"))
 (package-initialize)
 
 (when (not package-archive-contents)
